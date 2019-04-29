793bab420f0275f47455d5d5bdbfbe9f0e753642
Dipanwita Guhathakurta <42820001+susiejojo@users.noreply.github.com>
2019-04-30 01:59:29 +0530

    Corrected youtube video downloader

--- playlist.sh
+++ playlist.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
+#make sure you are saving the downloaded playlist to ~/Music/Playlist/
+youtube-dl --verbose -citk https://www.youtube.com/watch\?v\=Ho32Oh6b4jc\&list\=PLnl9aWBgMdHuBi1sTDhWG7I9KoGJqsnxS\&index\=3
 vlc ~/Music/Playlist/*.mp4
  

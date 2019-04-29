fea6f6b0a951bc7ef6bde00b5c6ac22245f9001f
Dipanwita Guhathakurta <42820001+susiejojo@users.noreply.github.com>
2019-04-30 02:02:15 +0530

    Time notifier

--- timer.sh
+++ timer.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+#displays current time,day and date
+#make sure u have a clock.png saved in ~/Pictures/
 icon="~/Pictures/clock.png"
 time="$(date +%H:%M)"
 text="Time: $(date +%T)               Date: $(date +%D), $(date +%A)"
